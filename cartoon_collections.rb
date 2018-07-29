2a528b257bffad5ea0be7f78cbad2548d02b087a
Benjamin Jones <bljones36@gmail.com>
2018-07-29 18:29:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
+#def roll_call_dwarves(array)
+#  i = 0
+#  while i < array.length
+#    puts "#{i+1}. #{array[i]}"
+#    i += 1
+#  end
+#end
+
 def roll_call_dwarves(array)
-  i = 0
-  while i < array.length
-    puts "#{i+1}. #{array[i]}"
-    i += 1
-  end
+    array.each_with_index
 end
 
 def summon_captain_planet# code an argument here
